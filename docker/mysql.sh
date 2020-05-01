7aed6a4a8b6bcd69e472d9af1aa32f1db6d36953
pjt3591oo <pjt3591oo@gmail.com>
2020-05-01 12:24:58 +0900

    컨테이너 이름을 변경한다

--- docker/mysql.sh
+++ docker/mysql.sh
@@ -1,16 +1,16 @@
 # root/password
 # 포트 3306 오픈
 # mysql 버전 5.7 => 8.x 버전은 node.js의 mysql 라이브러리 호환 불가
-docker run -p 3306:3306 --name mysql_exchange -e MYSQL_ROOT_PASSWORD=password -d mysql:5.7 mysqld
+docker run -p 3306:3306 --name mysql_movie -e MYSQL_ROOT_PASSWORD=password -d mysql:5.7 mysqld
 
 # 컨테이너 접속
-docker exec -it mysql_exchange /bin/bash
+docker exec -it mysql_movie /bin/bash
 
 # 컨테이너 프로세스 실행
-docker start mysql_exchange
+docker start mysql_movie
 
 # 컨테이너 프로세스 일시중지
-docker stop mysql_exchange
+docker stop mysql_movie
 
 # 컨테이너 프로세스 삭제
-docker rm mysql_exchange
+docker rm mysql_movie
--- docker/redis.sh
+++ docker/redis.sh
@@ -1,14 +1,14 @@
 # 포트 6379 오픈
-docker run --name redis_exchange -d -p 6379:6379 redis
+docker run --name redis_movie -d -p 6379:6379 redis
 
 # 컨테이너 접속
-docker exec -it redis_exchange /bin/bash
+docker exec -it redis_movie /bin/bash
 
 # 컨테이너 프로세스 실행
-docker start redis_exchange
+docker start redis_movie
 
 # 컨테이너 프로세스 일시중지
-docker stop redis_exchange
+docker stop redis_movie
 
 # 컨테이너 프로세스 삭제
-docker rm redis_exchange
+docker rm redis_movie
